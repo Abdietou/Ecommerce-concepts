c06c0210bded12f2b1635f9377a826130e401026
Abdietou <a.doucoure@ymail.com>
2020-06-28 14:33:28 +0200

    Entities + Repository + test CommandLineRunner

--- src/main/java/org/sid/lightecom/LightEcomApplication.java
+++ src/main/java/org/sid/lightecom/LightEcomApplication.java
@@ -1,13 +1,48 @@
 package org.sid.lightecom;
 
+import net.bytebuddy.utility.RandomString;
+import org.sid.lightecom.dao.CategoryRepository;
+import org.sid.lightecom.dao.ProductRepository;
+import org.sid.lightecom.entities.Category;
+import org.sid.lightecom.entities.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Random;
+
 @SpringBootApplication
-public class LightEcomApplication {
+public class LightEcomApplication implements CommandLineRunner {
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(LightEcomApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        categoryRepository.save(new Category(null, "Ordinateurs", null, null));
+        categoryRepository.save(new Category(null, "Imprimante", null, null));
+        categoryRepository.save(new Category(null, "Smartphone", null, null));
+
+        Random rnd= new Random();
+        categoryRepository.findAll().forEach(c-> {
+            for (int i = 0; i < 10; i++) {
+                Product p = new Product();
+                p.setNom(RandomString.make(18));
+                p.setCurrentPrice(100 + rnd.nextInt(100000));
+                p.setAvailable(rnd.nextBoolean());
+                p.setPromotion(rnd.nextBoolean());
+                p.setSelected(rnd.nextBoolean());
+                p.setCategory(c);
+                productRepository.save(p);
+            }
+
+        });
     }
 
 }
---
+++ src/main/java/org/sid/lightecom/dao/ProductRepository.java
@@ -0,0 +1,17 @@
+package org.sid.lightecom.dao;
+
+import org.sid.lightecom.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    @RestResource(path = "/selectedProducts")
+    public List<Product> findBySelectedIsTrue();
+    @RestResource(path = "/productsByKeyword")
+    public List<Product> findByNomContains(@Param("mc") String mc);
+}
